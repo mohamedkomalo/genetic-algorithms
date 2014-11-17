b1d17ff502fb38ba2103a45e5c5022839c936570
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-11-17 02:07:54 +0200

    implemeneted crossover

--- src/komalo/genetic_algorithms/CanonicalGeneticAlgorithm.java
+++ src/komalo/genetic_algorithms/CanonicalGeneticAlgorithm.java
@@ -12,7 +12,7 @@ public class CanonicalGeneticAlgorithm<SolutionType, GeneType> {
 	private ChromosomeCodec<SolutionType, GeneType> chromosomeCodec;
 
 	private GeneMutator<GeneType> geneMutator;
-	
+
 	private FitnessFunction<SolutionType> fitnessFunction;
 
 	private int iterationsNo;
@@ -27,9 +27,10 @@ public class CanonicalGeneticAlgorithm<SolutionType, GeneType> {
 
 	public CanonicalGeneticAlgorithm(double populationNo, int iterationsNo,
 			double mutationProbability, double crossOverProbability,
-			FitnessFunction<SolutionType> fitnessFunction, ChromosomeCodec<SolutionType, GeneType> chromosomeCodec,
+			FitnessFunction<SolutionType> fitnessFunction,
+			ChromosomeCodec<SolutionType, GeneType> chromosomeCodec,
 			GeneMutator<GeneType> chromosomeMutator) {
-		
+
 		this.populationNo = populationNo;
 		this.iterationsNo = iterationsNo;
 		this.fitnessFunction = fitnessFunction;
@@ -41,16 +42,16 @@ public class CanonicalGeneticAlgorithm<SolutionType, GeneType> {
 
 	public void solve() {
 		population.clear();
-		
+
 		generateRandomPopulation();
-		
+
 		for (int i = 0; i < iterationsNo; i++) {
 			ChromosomePair<GeneType> parents = selectParents();
 
 			ChromosomePair<GeneType> offSprings = crossover(parents);
-			
+
 			mutateChromosomeInPlace(offSprings.chromosome1);
-			
+
 			mutateChromosomeInPlace(offSprings.chromosome2);
 
 			replaceInPopulation(parents, offSprings);
@@ -60,45 +61,70 @@ public class CanonicalGeneticAlgorithm<SolutionType, GeneType> {
 	private ChromosomePair<GeneType> selectParents() {
 		List<Integer> cummulativeFitness = new LinkedList<>();
 		List<Chromosome<GeneType>> chromosomes = new LinkedList<>();
-		
+
 		int lastFitness = 0;
-		
-		for(Chromosome<GeneType> chromosome : population){
+
+		for (Chromosome<GeneType> chromosome : population) {
 			SolutionType decoded = chromosomeCodec.decode(chromosome);
 			int fitness = fitnessFunction.evaluate(decoded);
-			
+
 			cummulativeFitness.add(fitness + lastFitness);
 			chromosomes.add(chromosome);
 		}
-		
-		Chromosome<GeneType> parent1 = selectRandomParent(cummulativeFitness, chromosomes);
-		
+
+		Chromosome<GeneType> parent1 = selectRandomParent(cummulativeFitness,
+				chromosomes);
+
 		Chromosome<GeneType> parent2 = parent1;
-		
-		while(parent1 == parent2)
+
+		while (parent1 == parent2)
 			parent2 = selectRandomParent(cummulativeFitness, chromosomes);
-		
+
 		return new ChromosomePair<>(parent1, parent2);
 	}
 
-	private Chromosome<GeneType> selectRandomParent(List<Integer> cummulativeFitness,
+	private Chromosome<GeneType> selectRandomParent(
+			List<Integer> cummulativeFitness,
 			List<Chromosome<GeneType>> chromosomes) {
-		
-		int totalFitness = cummulativeFitness.get(cummulativeFitness.size() - 1);
-		
+
+		int totalFitness = cummulativeFitness
+				.get(cummulativeFitness.size() - 1);
+
 		int randomNumber = randomGenerator.nextInt(totalFitness);
-		
-		for(int i=0; i < cummulativeFitness.size() - 1; i++){
-			if(randomNumber < cummulativeFitness.get(i+1)){
+
+		for (int i = 0; i < cummulativeFitness.size() - 1; i++) {
+			if (randomNumber < cummulativeFitness.get(i + 1)) {
 				return chromosomes.get(i);
 			}
 		}
-		
-		throw new RuntimeException("could not select a valid parent from the fitness list");
+
+		throw new RuntimeException(
+				"could not select a valid parent from the fitness list");
 	}
 
 	private ChromosomePair<GeneType> crossover(ChromosomePair<GeneType> parents) {
-		return null;
+		int chromosomLength = parents.chromosome1.length();
+		int randomCrossoverPoint = randomGenerator.nextInt(chromosomLength);
+
+		Chromosome<GeneType> offSpring1 = new Chromosome<GeneType>(chromosomLength);
+		for (int i = 0; i < randomCrossoverPoint; i++){
+			offSpring1.setGeneAt(i, parents.chromosome1.getGeneAt(i));
+		}
+
+		for (int i = randomCrossoverPoint; i < chromosomLength; i++){
+			offSpring1.setGeneAt(i, parents.chromosome2.getGeneAt(i));
+		}
+
+		Chromosome<GeneType> offSpring2 = new Chromosome<GeneType>(chromosomLength);
+		for (int i = 0; i < randomCrossoverPoint; i++){
+			offSpring2.setGeneAt(i, parents.chromosome2.getGeneAt(i));
+		}
+
+		for (int i = randomCrossoverPoint; i < chromosomLength; i++){
+			offSpring2.setGeneAt(i, parents.chromosome1.getGeneAt(i));
+		}
+		
+		return new ChromosomePair<>(offSpring1, offSpring2);
 	}
 
 	private void replaceInPopulation(ChromosomePair<GeneType> parents,
@@ -109,18 +135,19 @@ public class CanonicalGeneticAlgorithm<SolutionType, GeneType> {
 		population.add(offSprings.chromosome1);
 		population.add(offSprings.chromosome2);
 	}
-	
-	private void generateRandomPopulation(){
+
+	private void generateRandomPopulation() {
 		throw new RuntimeException("Not implemented");
 	}
-	
-	public void mutateChromosomeInPlace(Chromosome<GeneType> chromosome){
-		for(int i=0; i<chromosome.length(); i++){
+
+	public void mutateChromosomeInPlace(Chromosome<GeneType> chromosome) {
+		for (int i = 0; i < chromosome.length(); i++) {
 			double randomNumber = randomGenerator.nextDouble();
-			
-			if(randomNumber < mutationProbability){
-				GeneType newGene = geneMutator.mutuateGene(chromosome.getGeneAt(i));
-				
+
+			if (randomNumber < mutationProbability) {
+				GeneType newGene = geneMutator.mutuateGene(chromosome
+						.getGeneAt(i));
+
 				chromosome.setGeneAt(i, newGene);
 			}
 		}
@@ -129,7 +156,7 @@ public class CanonicalGeneticAlgorithm<SolutionType, GeneType> {
 	private static class ChromosomePair<GeneType> {
 		Chromosome<GeneType> chromosome1;
 		Chromosome<GeneType> chromosome2;
-		
+
 		public ChromosomePair(Chromosome<GeneType> chromosome1,
 				Chromosome<GeneType> chromosome2) {
 			super();
